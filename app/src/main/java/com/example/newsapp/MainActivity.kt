da2abf643efdaaedf390fc50140df60cd9ee9fe0
Pranat <praveer.pranat2@gmail.com>
2022-02-06 17:30:33 +0530

    Second commit

--- app/src/main/java/com/example/newsapp/MainActivity.kt
+++ app/src/main/java/com/example/newsapp/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.newsapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.SearchView
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -10,19 +12,41 @@ import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     lateinit var recyclerView: RecyclerView
+    lateinit var search: SearchView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         recyclerView=findViewById(R.id.recyclerView)
+        search=findViewById(R.id.searchView)
         val MainViewModel:MainViewModel= ViewModelProvider(this).get(MainViewModel::class.java)
         MainViewModel.getNews()
-        MainViewModel.mdata.observe(this, Observer {
-            Log.d("a",it.title)
-            val NewsAdapter = NewsAdapter(this,MainViewModel.author,MainViewModel.url,MainViewModel.urltoimage,MainViewModel.title,MainViewModel.time)
+        MainViewModel.Article.observe(this, Observer {
+            Log.d("Articles", MainViewModel.Article.value?.get(3)?.title.toString())
+            val NewsAdapter = NewsAdapter(this,MainViewModel.Article)
             recyclerView.layoutManager = LinearLayoutManager(this)
             recyclerView.adapter = NewsAdapter
             recyclerView.setHasFixedSize(true)
         })
-    }
 
+        search.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                return true
+            }
+            override fun onQueryTextChange(newText: String?): Boolean {
+                //MainViewModel.Article.value?.filter { it.title == newText }
+                MainViewModel.Article.value?.forEach {
+                    if(it.title==newText){
+                        MainViewModel.Article.value
+                    }
+
+                }
+                recyclerView.adapter?.notifyDataSetChanged()
+                   // MainViewModel.Article.value=MainViewModel.Article.value?.filter {
+                     //   it.title == newText
+                    //}
+                return true
+            }
+    })
+
+}
 }
--- app/src/main/java/com/example/newsapp/NewsAdapter.kt
+++ app/src/main/java/com/example/newsapp/NewsAdapter.kt
@@ -8,12 +8,20 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.browser.customtabs.CustomTabColorSchemeParams
+import androidx.browser.customtabs.CustomTabsIntent
 import androidx.cardview.widget.CardView
 import androidx.core.content.ContextCompat
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 
-class NewsAdapter(val context: Context,val author: ArrayList<String>,val url:ArrayList<String>,val urltoimage:ArrayList<String>, val title:MutableList<String>, var time:ArrayList<String>):RecyclerView.Adapter<NewsAdapter.NewsViewHolder>() {
+class NewsAdapter(
+    val context: Context,
+    var article: MutableLiveData<List<Article>>
+):RecyclerView.Adapter<NewsAdapter.NewsViewHolder>() {
+
     inner class NewsViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         var tv_title = itemView.findViewById<TextView>(R.id.tv_title)
         var news_image = itemView.findViewById<ImageView>(R.id.news_image)
@@ -21,6 +29,7 @@ class NewsAdapter(val context: Context,val author: ArrayList<String>,val url:Arr
         var tvAuthor = itemView.findViewById<TextView>(R.id.tvAuthor)
         var tvtime = itemView.findViewById<TextView>(R.id.tvtime)
         var favouriteimage = itemView.findViewById<ImageView>(R.id.favouriteimage)
+
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NewsAdapter.NewsViewHolder {
@@ -29,24 +38,20 @@ class NewsAdapter(val context: Context,val author: ArrayList<String>,val url:Arr
     }
 
     override fun onBindViewHolder(holder: NewsAdapter.NewsViewHolder, position: Int) {
-
-        holder.tv_title.text = title[position]
-        Glide.with(context).load(urltoimage[position]).placeholder(R.drawable.ic_imageload)
+        holder.tv_title.text = article.value?.get(position)?.title
+        Glide.with(context).load(article.value?.get(position)?.urlToImage).placeholder(R.drawable.ic_imageload)
             .into(holder.news_image)
-        holder.tvAuthor.text = "By-" + author[position]
-        holder.tvtime.text = "-" + time[position]
+        holder.tvAuthor.text = "By-" + article.value?.get(position)?.author
+        holder.tvtime.text = "-" + article.value?.get(position)?.publishedAt
         holder.cardview.setOnClickListener {
-            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url[position]))
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(article.value?.get(position)?.url))
             context.startActivity(intent)
         }
         holder.favouriteimage.setOnClickListener {
-                holder.favouriteimage.setImageDrawable(
-                    ContextCompat.getDrawable(
-                        holder.favouriteimage.context,
-                        R.drawable.ic_clicked_star))
+            holder.favouriteimage.setImageDrawable(ContextCompat.getDrawable(holder.favouriteimage.context,R.drawable.ic_clicked_star))
         }
     }
     override fun getItemCount(): Int {
-            return title.size
+            return article.value?.size!!
         }
 }
